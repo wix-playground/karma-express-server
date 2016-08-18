75b1bef669e2c4a280d0b8c1f818d5bbda066d4d
Amir Arad <amira@wix.com>
2016-08-18 15:45:26 +0300

    close express server on karma exit

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ const https = require('https');
 const bodyParser = require('body-parser');
 
 if (process.env.NODE_ENV !== 'production') {
-  require('longjohn');
+    require('longjohn');
 }
 
 /**
@@ -13,88 +13,91 @@ if (process.env.NODE_ENV !== 'production') {
  */
 
 module.exports = {
-  'framework:expressServer': ['factory',
+    'framework:expressServer': ['factory',
 
-    /**
-     * @function framework
-     * @memberOf karmaExpressServer
-     * @description
-     * Creates an express server and passes `app` and `logger` to karma
-     * configuration for further extension.
-     * @param {Array} [args=[]] args not currently used
-     * @param {karmaExpressServer.Config} config
-     * @param {Karma.Plugin.Logger} logger [Official Docs]{@link http://karma-runner.github.io/0.13/dev/plugins.html}
-     */
-    function framework(args, config, logger, helper) {
-      // default config
-      config = config || {};
-      config = config.expressServer || {};
-      var configDefaults = {
-        callback: function(){},
-        accessControlAllowOrigin: 'http://localhost:9876',
-        accessControlAllowCredentials: 'true',
-        accessControlAllowMethods: 'GET, PUT, POST, DELETE, OPTIONS',
-        accessControlMaxAge: '3600',
-        accessControlAllowHeaders: 'x-requested-with, Content-Type, ' +
-          'Content-Length, Content-Range, Content-Encoding',
-        protocol: 'http',
-        httpsServerOptions: null,
-        serverPort: 9877,
-        extensions: []
-      };
-      var conf = {};
-      Object.assign(conf, configDefaults, config);
+        /**
+         * @function framework
+         * @memberOf karmaExpressServer
+         * @description
+         * Creates an express server and passes `app` and `logger` to karma
+         * configuration for further extension.
+         * @param {Array} [args=[]] args not currently used
+         * @param {karmaExpressServer.Config} config
+         * @param {Karma.Plugin.Logger} logger [Official Docs]{@link http://karma-runner.github.io/0.13/dev/plugins.html}
+         */
+            function framework(args, config, logger, emitter) {
+            // default config
+            config = config || {};
+            config = config.expressServer || {};
+            var configDefaults = {
+                callback: function(){},
+                accessControlAllowOrigin: 'http://localhost:9876',
+                accessControlAllowCredentials: 'true',
+                accessControlAllowMethods: 'GET, PUT, POST, DELETE, OPTIONS',
+                accessControlMaxAge: '3600',
+                accessControlAllowHeaders: 'x-requested-with, Content-Type, ' +
+                'Content-Length, Content-Range, Content-Encoding',
+                protocol: 'http',
+                httpsServerOptions: null,
+                serverPort: 9877,
+                extensions: []
+            };
+            var conf = {};
+            Object.assign(conf, configDefaults, config);
 
-      // input validation
-      if (
-        conf.protocol === 'https' && (
-          conf.httpsServerOptions === null ||
-          conf.httpsServerOptions === undefined
-        )
-      ) {
-        throw new Error('Illegal argument: `httpsServerOptions` cannot be null or undefined.');
-      }
+            // input validation
+            if (
+                conf.protocol === 'https' && (
+                    conf.httpsServerOptions === null ||
+                    conf.httpsServerOptions === undefined
+                )
+            ) {
+                throw new Error('Illegal argument: `httpsServerOptions` cannot be null or undefined.');
+            }
 
-      // construct express server
-      var log = logger.create('karma-express-server');
-      log.info('Starting express server...');
-      var app = express();
-      app.use(bodyParser.json());
-      app.use(function(req, res, next) {
-        res.set('Access-Control-Allow-Credentials',
-            conf.accessControlAllowCredentials.toString());
-        res.set('Access-Control-Allow-Methods',
-            conf.accessControlAllowMethods);
-        res.set('Access-Control-Max-Age', conf.accessControlMaxAge);
-        res.set('Access-Control-Allow-Headers', conf.accessControlAllowHeaders);
-        res.set('Access-Control-Allow-Origin', conf.accessControlAllowOrigin);
-        next();
-      });
+            // construct express server
+            var log = logger.create('karma-express-server');
+            log.info('Starting express server...');
+            var app = express();
+            app.use(bodyParser.json());
+            app.use(function(req, res, next) {
+                res.set('Access-Control-Allow-Credentials',
+                    conf.accessControlAllowCredentials.toString());
+                res.set('Access-Control-Allow-Methods',
+                    conf.accessControlAllowMethods);
+                res.set('Access-Control-Max-Age', conf.accessControlMaxAge);
+                res.set('Access-Control-Allow-Headers', conf.accessControlAllowHeaders);
+                res.set('Access-Control-Allow-Origin', conf.accessControlAllowOrigin);
+                next();
+            });
 
-      // extend express application
-      conf.extensions.forEach(function(extension) {
-        extension(app, logger);
-      });
-
-      // start express server
-      if (conf.protocol === 'https') {
-        var httpServer = https
-        .createServer(conf.httpsServerOptions, app)
-        .listen(conf.serverPort, function(){
-          log.info('Listening on port %d...', conf.serverPort);
-          conf.callback(httpServer);
-        });
-      } else {
-        var httpServer = http
-        .createServer(app)
-        .listen(conf.serverPort, function(){
-          log.info('Listening on port %d...', conf.serverPort);
-          conf.callback(httpServer);
-        });
-      }
-
-    }
-  ]
+            // extend express application
+            conf.extensions.forEach(function(extension) {
+                extension(app, logger);
+            });
+            var httpServer;
+            // start express server
+            if (conf.protocol === 'https') {
+                httpServer = https
+                    .createServer(conf.httpsServerOptions, app)
+                    .listen(conf.serverPort, function(){
+                        log.info('Listening on port %d...', conf.serverPort);
+                        conf.callback(httpServer);
+                    });
+            } else {
+                httpServer = http
+                    .createServer(app)
+                    .listen(conf.serverPort, function(){
+                        log.info('Listening on port %d...', conf.serverPort);
+                        conf.callback(httpServer);
+                    });
+            }
+            emitter.on('run_complete', function(){
+                log.info('closing time');
+                httpServer.close();
+            });
+        }
+    ]
 };
 
 /**
